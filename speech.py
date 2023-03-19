581a693b4f0b611c2ba0072fc622dab1410c4afc
OrhanBC <67674376+OrhanBC@users.noreply.github.com>
2023-03-18 20:32:20 -0400

    Update speech.py

--- speech.py
+++ speech.py
@@ -5,28 +5,26 @@ def speech_to_text():
     r = sr.Recognizer()
 
 
-    while True:
-        # use the microphone as the audio source
-        with sr.Microphone() as source:
-            print("Speak now...")
-            # adjust the ambient noise level to account for background noise
-            r.adjust_for_ambient_noise(source)
-            # listen for the user's voice input
-            audio = r.listen(source)
-            
+   
+       # use the microphone as the audio source
+       with sr.Microphone() as source:
+           print("Speak now...")
+           # adjust the ambient noise level to account for background noise
+           r.adjust_for_ambient_noise(source)
+           # listen for the user's voice input
+           audio = r.listen(source)
+           
+       try:
+           # recognize speech using Google Speech Recognition
+           text = r.recognize_google(audio)
+           print("You said: {}".format(text))
+   
+       except sr.UnknownValueError:
+           print("Sorry, I didn't understand that.")
+       except sr.RequestError:
+           print("Sorry, could not request results from Google Speech Recognition.")
 
-        try:
-            # recognize speech using Google Speech Recognition
-            text = r.recognize_google(audio)
-            print("You said: {}".format(text))
-    
-        except sr.UnknownValueError:
-            print("Sorry, I didn't understand that.")
-    
-        except sr.RequestError:
-            print("Sorry, could not request results from Google Speech Recognition.")
-
-        if (input("Would you like to again?(Y/N)")=="N"):
-            return text
+        
+        return text
 
 
