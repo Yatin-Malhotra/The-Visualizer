83a131c6f995c2e8085792a0d389fc168ce71075
OrhanBC <67674376+OrhanBC@users.noreply.github.com>
2023-03-18 22:09:03 -0400

    Update tts.py

--- tts.py
+++ tts.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import ImageTk, Image
 import speech as sp
 import description as ds
 import process as ps
@@ -16,9 +17,9 @@ label.pack(pady=20)
 
 # Create a function to handle button clicks
 def handle_button_click():
-    # Hide the main window
-    window.withdraw()
-    
+    # # Hide the main window
+    # window.withdraw()
+
     # Create a new Tkinter window
     speak_window = tk.Toplevel()
 
@@ -29,17 +30,18 @@ def handle_button_click():
     # Create a label widget to display text
     speak_label = tk.Label(speak_window, text="Speak into the microphone", font=("Arial", 16))
     speak_label.pack(pady=20)
-    
+
     # Take input as speech
-    text = sp.speech_to_text()
-    
+    ip_text = sp.speech_to_text()
+
     # Give use that to get input for the other file
-    input_text = ds.generate_description(text)
-    sentiments = ds.get_sentiments(text)
-    
+    input_text = ds.generate_description(ip_text)
+    sentiments = ds.get_sentiments(ip_text)
+
     # Create a function to handle "Done" button clicks
     def handle_done_button_click():
         # Close the speak window
+        window.withdraw()
         speak_window.withdraw()
 
         # Create a new Tkinter window
@@ -47,15 +49,25 @@ def handle_button_click():
 
         # Set the window title and size
         image_window.title("Image Window")
-        image_window.geometry("400x200")
+        image_window.geometry("400x400")
 
         # Create a label widget to display text
-        image_label = tk.Label(image_window, text="Here's a rendered image", font=("Arial", 16))
+        image_label = tk.Label(image_window, text="Here's a rendered image\n(You spoke: " + ip_text + ")", font=("Arial", 16))
         image_label.pack(pady=20)
-        
+
         # Generate image
         ps.generate_image(input_text, sentiments)
-        
+
+        img = Image.open('image.png')
+        img = img.resize((350, 350))
+        # convert the image to a PhotoImage object
+        photo = ImageTk.PhotoImage(img)
+
+        # create a label with the image
+        render_label = tk.Label(image_window, image=photo)
+        render_label.image = photo
+        render_label.pack(pady=20)
+
         # Create a function to handle "Quit" button clicks
         def handle_image_quit_button_click():
             # Close the image window
@@ -63,11 +75,11 @@ def handle_button_click():
 
             # Show the main window again
             window.deiconify()
-            
+
         # Create a quit button widget
         image_quit_button = tk.Button(image_window, text="Quit", font=("Arial", 14), command=handle_image_quit_button_click)
         image_quit_button.pack(pady=10)
-    
+
     # Create a function to handle "Quit" button clicks
     def handle_speak_quit_button_click():
         # Close the speak window
@@ -79,7 +91,7 @@ def handle_button_click():
     # Create a button widget to quit the current UI window and open the image window
     done_button = tk.Button(speak_window, text="Done", font=("Arial", 14), command=handle_done_button_click)
     done_button.pack(pady=10)
-    
+
     # Create a quit button widget to go back to the main window
     speak_quit_button = tk.Button(speak_window, text="Quit", font=("Arial", 14), command=handle_speak_quit_button_click)
     speak_quit_button.pack(pady=10)
